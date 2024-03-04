a1c658c76c95c436680f8ab273f7f5a9a1a05154
sedep040508 <elikbaevasedeppka@gmail.com>
2024-03-04 14:57:33 +0600

    added filter

--- js/core.js
+++ js/core.js
@@ -1,63 +1,10 @@
 const filterModel = document.querySelector('.filter_model')
+const filterMark = document.querySelector('.filter_mark')
+const  filterYear = document.getElementById('.year')
 const cardsContainer = document.querySelector('.cards')
 
-const arrFromMark = []
-cards.map(el => {
-  arrFromMark.push(el.mark.toUpperCase().trimEnd().trimStart())
-})
-const newArrFromMark = Array.from(new Set(arrFromMark))
-newArrFromMark.map(el => {
-  let btn = `
-  <button class="btn model">${el}</button>
-  `
-  filterModel.insertAdjacentHTML('beforeend', btn)
-})
-const modelBtns = document.querySelectorAll('.model')
-modelBtns.forEach(el => {
-  el.addEventListener('click', function(){
-    document.querySelector('.model.active').classList.remove('active')
-    this.classList.add('active')
-    const arrFromMark = []
-cards.map(el => {
-  arrFromMark.push(el.mark.toUpperCase().trimEnd().trimStart())
-})
-const newArrFromMark = Array.from(new Set(arrFromMark))
-newArrFromMark.map(el => {
-  let btn = `
-  <button class="btn mark">${el}</button>
-  `
-  filterModel.insertAdjacentHTML('beforeend', btn)
-})
-  })
-})
-
-const btnFilter = document.querySelector('.filter_btn')
-btnFilter.addEventListener('click', function(){
-  cardsContainer.innerHTML = ''
-  const activeBtnMark = document.querySelector('.model.active')
-  if(activeBtnMark.innerText === "ВСЕ"){
-     cards.map(el => {
-        addCard(el)
-      cardsContainer.insertAdjacentHTML('beforeend', card)
-    })
-  }else{
-    cards.map(el => {
-      if(activeBtnMark.innerText === el.mark.toUpperCase().trimEnd().trimStart()){
-        addCard(el)
-        cardsContainer.insertAdjacentHTML('beforeend', card)
-      }
-    })
-  }
-})
-
-
-cards.map(el => {
-    addCard(el)
-})
-
-
-function addCard(el){
-    let card = `
+function addCards(el) {
+  let card = `
   <div class="card">
     <div class="card_img">
       <img src=${el.img} alt="">
@@ -74,9 +21,86 @@ function addCard(el){
   </div>
   `
   cardsContainer.insertAdjacentHTML('beforeend', card)
-
 }
+//
+const arrFromMark = []
+cards.map(el => {
+  arrFromMark.push(el.mark.toUpperCase().trimEnd().trimStart())
+})
+const newArrFromMark = Array.from(new Set(arrFromMark))
+newArrFromMark.map(el => {
+  let btn = `
+  <button class="btn model">${el}</button>
+  `
+  filterModel.insertAdjacentHTML('beforeend', btn)
+})
+// фильтрация по моделям
+const modelBtns = document.querySelectorAll('.model')
+modelBtns.forEach(el => {
+  el.addEventListener('click', function () {
+    document.querySelector('.model.active').classList.remove('active')
+    this.classList.add('active')
+    const arrFromModel = []
+    cardsContainer.innerHTML = ''
+    cards.map(el => {
+      if (this.innerText === el.mark.toUpperCase().trimEnd().trimStart()) {
+        arrFromModel.push(el.model.toUpperCase().trimEnd().trimStart())
+        addCards(el)
+      } else if (this.innerText === "ВСЕ") {
+        addCards(el)
+      }
+    })
+    filterMark.innerHTML = `<button class="btn mark">ВСЕ</button>`
+    const newArrFromModel = Array.from(new Set(arrFromModel))
+    newArrFromModel.map(el => {
+      let btn = `
+      <button class="btn mark">${el}</button>
+      `
+      filterMark.insertAdjacentHTML('beforeend', btn)
+    })
+
+    if (this.innerText != "ВСЕ") {
+      document.querySelector('.filter_mark-wrap').style.display = "block"
+    } else {
+      document.querySelector('.filter_mark-wrap').style.display = "none"
+    }
+//год
+
+
+    const markBtns = document.querySelectorAll('.mark')
+    markBtns.forEach(el => {
+      el.addEventListener('click', function () {
+        cardsContainer.innerHTML = ''
+        cards.map(el => {
+          if (this.innerText === el.model.toUpperCase().trimEnd().trimStart()) {
+            addCards(el)
+          } else if (this.innerText === "ВСЕ") {
+            addCards(el)
+          }
+        })
+      })
+    })
+  })
+})
+
+
+// const btnFilter = document.querySelector('.filter_btn')
+// btnFilter.addEventListener('click', function(){
+//   cardsContainer.innerHTML = ''
+//   const activeBtnMark = document.querySelector('.model.active')
+//   if(activeBtnMark.innerText === "ВСЕ"){
+//      cards.map(el => {
+//       addCards(el)
+//     })
+//   }else{
+//     cards.map(el => {
+//       if(activeBtnMark.innerText === el.mark.toUpperCase().trimEnd().trimStart()){
+//         addCards(el)
+//       }
+//     })
+//   }
+// })
 
 cards.map(el => {
-    addCard(el)
+  addCards(el)
 })
